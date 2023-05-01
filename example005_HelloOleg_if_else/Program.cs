e4a4df47580c5de13dd8c8e5964766d87203f290
Daniil Zavarin <daniil1.spb@mail.ru>
2023-05-02 02:23:40 +0300

    add example005_hellooleg_if_else

---
+++ example005_HelloOleg_if_else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "олег" )
+{
+    Console.WriteLine("Ха реально Олег кто тебе имя такое придумал. Ржомба");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
