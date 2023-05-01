b84803303cfe53ff3661966e631b14d8fabb99b1
Daniil Zavarin <daniil1.spb@mail.ru>
2023-05-02 01:56:27 +0300

    update example004 div random

--- example004_div/Program.cs
+++ example004_div/Program.cs
@@ -1,4 +1,6 @@
-﻿double namberA = 12;
-double namberB = 5;
-double result = namberA / namberB;
+﻿double namberA = new Random().Next(1, 10); // 1 2 3 4 ... 9
+Console.WriteLine(namberA);
+double namberB = new Random().Next(1, 10);
+Console.WriteLine(namberB);
+double result = namberA + namberB;
 Console.WriteLine(result);
