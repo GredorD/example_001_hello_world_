641af7ddb49005d7047a74c5752b95d669e59286
Daniil Zavarin <daniil1.spb@mail.ru>
2023-05-02 18:45:45 +0300

    add homework002

---
+++ Homework002/Program.cs
@@ -0,0 +1,25 @@
+﻿int max = 0;
+
+Console.Write("add first number ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("add second number ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("add third number ");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+
+if(firstNumber > max)
+{
+    max = firstNumber;
+}
+if(secondNumber > max)
+{
+    max = secondNumber;
+}
+if(thirdNumber > max)
+{
+    max = thirdNumber;
+}
+
+Console.WriteLine("max = " + max);
