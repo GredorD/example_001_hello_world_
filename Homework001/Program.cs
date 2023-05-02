e5f9eed3aa2fcdec217e6419910bc3fe4cc2047b
Daniil Zavarin <daniil1.spb@mail.ru>
2023-05-02 19:49:49 +0300

    updatehomework 001

--- Homework001/Program.cs
+++ Homework001/Program.cs
@@ -1,20 +1,28 @@
-﻿int max = 0;
-int min = 0;
+﻿// int max = 0;
+// int min = 0;
 
-Console.Write("Enter the first number ");
-int firstNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the first number ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter the second number ");
-int secondNumber = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Enter the second number ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
 
-if(firstNumber > secondNumber)
-{
-    max = firstNumber;
-    min = secondNumber;
-}
-else
-{
-    max = secondNumber;
-    min = firstNumber;
-}
-Console.WriteLine("max = " + max + " and " + "min = " + min); 
+// if(firstNumber > secondNumber)
+// {
+//     max = firstNumber; min = secondNumber;
+// }
+// else
+// {
+//     max = secondNumber; min = firstNumber;
+// }
+// Console.WriteLine("max = " + max + " and " + "min = " + min); 
+
+Console.WriteLine("Введите первое число:");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число:");
+
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(a > b ? "Первое число больше второго" : "Второе число больше первого");
