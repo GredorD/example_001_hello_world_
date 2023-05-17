936e8a44e7c83d39a2b5c8ba481409b357809a30
Daniil Zavarin <daniil1.spb@mail.ru>
2023-05-17 21:40:33 +0300

    ffff

---
+++ homework005nostring/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Enter a 3-digit number:");
+try {
+	int Value = Convert.ToInt32(Console.ReadLine());
+	int Length = Value.ToString().Length;
+	if (Length == 3) {
+		int result = ( Value/10 ) % 10;
+		Console.WriteLine($"The second digit is {result}");
+	} else {
+		Console.WriteLine("Not a 3-digit number");
+	}
+} catch (System.Threading.ThreadAbortException) {
+	System.Threading.Thread.ResetAbort();
+} catch (Exception e) {
+	Console.WriteLine(e.Message);
+}
